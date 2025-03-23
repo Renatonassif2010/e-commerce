f6c83dfc7ef815897ccb24455124436f911011af
Renato Nassif <renatonassif2010@gmail.com>
2025-03-23 12:17:19 -0300

    New commit

---
+++ js/animationH2.js
@@ -0,0 +1,19 @@
+const h2 = document.getElementById("h2");
+function animationH2() {
+  let time;
+  let arrayWords = [
+    "Low price! Buy ours products!",
+    "Oportunity to buy!",
+    "Quality in products!",
+    "Good Service!",
+    "The best company in the technology!",
+  ];
+  time = setInterval(function () {
+    let number = Math.floor(Math.random() * arrayWords.length);
+    h2.style.opacity = 0;
+    setTimeout(function () {
+      h2.innerHTML = arrayWords[number];
+      h2.style.opacity = 1;
+    }, 1000);
+  }, 5000);
+}
